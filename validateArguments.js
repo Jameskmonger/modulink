fa7213279c2231e366ae5970c22b23ea844a227c
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-03-21 13:21:54 +0000

    Validate name

--- validateArguments.js
+++ validateArguments.js
@@ -1,7 +1,18 @@
 (function(mod) {
 
     mod.exports = function (args) {
-        
+
+        var issues = [];
+
+        if (!args.name) {
+            issues.push({
+                syntax: '--name PACKAGE_NAME',
+                example: '--name modulink'
+            });
+        }
+
+        return issues;
+
     };
 
 })(module);
