c16c489c0b2f1fb795b0390a6576b30cc832c969
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-03-21 13:36:33 +0000

    Add syntax command

--- syntax.js
+++ syntax.js
@@ -4,6 +4,10 @@
         {
             syntax: '--name PACKAGE_NAME',
             example: '--name modulink'
+        },
+        {
+            syntax: '--target DIRECTORY',
+            example: '--target src'
         }
     ];
 
