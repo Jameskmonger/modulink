a81cef52f20864b9227bc0f62dfe108f09c2dff1
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-03-21 13:34:18 +0000

    Store syntax

---
+++ syntax.js
@@ -0,0 +1,13 @@
+(function(mod) {
+
+    mod.exports = (function() {
+
+        return [
+            {
+                syntax: '--name PACKAGE_NAME',
+                example: '--name modulink'
+            }
+        ];
+    })();
+
+})(module);
